13f45f3b78e09a3de17ec1164d6158c2f96f3d61
DidiisdaGOAT <24581@burnside.school.nz>
2026-05-25 14:26:07 +1200

    Getting columms and names

--- code.py
+++ code.py
@@ -2,4 +2,14 @@ import sqlite3
 
 DATABASE = "Baskatball_stats"
 
-sql= 
+#Defining the functions
+
+db = sqlite3.connect("Basketball_stats")
+cursor = db.cursor()
+sql = "SELECT * FROM team"
+cursor.execute(sql)
+results = cursor.fetchall()
+print("Last Name               First Name    Jersey Number   PPG    Total Points")
+for player in results:
+    print(f"{player[1]:<24}{player[2]:<15}#{player[3]:<14}{player[4]:<8}{player[5]:<14}{player[6]:<6}{player[7]:<6}{player[8]}")
+db.close()    
