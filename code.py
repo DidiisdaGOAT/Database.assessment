5de630143f680cb0dfbdc598a38380287604e0ca
DidiisdaGOAT <24581@burnside.school.nz>
2026-05-25 14:32:33 +1200

    First Function

--- code.py
+++ code.py
@@ -3,13 +3,16 @@ import sqlite3
 DATABASE = "Baskatball_stats"
 
 #Defining the functions
+def print_all_players():
+    db = sqlite3.connect("Basketball_stats")
+    cursor = db.cursor()
+    sql = "SELECT * FROM team"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("Last Name               First Name    Jersey Number  PPG    Total  High  Fouls Games Played")
+    for player in results:
+        print(f"{player[1]:<24}{player[2]:<14}#{player[3]:<14}{player[4]:<8}{player[5]:<6}{player[6]:<6}{player[7]:<6}{player[8]}")
+    db.close()    
 
-db = sqlite3.connect("Basketball_stats")
-cursor = db.cursor()
-sql = "SELECT * FROM team"
-cursor.execute(sql)
-results = cursor.fetchall()
-print("Last Name               First Name    Jersey Number   PPG    Total Points")
-for player in results:
-    print(f"{player[1]:<24}{player[2]:<15}#{player[3]:<14}{player[4]:<8}{player[5]:<14}{player[6]:<6}{player[7]:<6}{player[8]}")
-db.close()    
+
+print_all_players()
